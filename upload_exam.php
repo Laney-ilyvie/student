fa9710539a26dcacf28a3c83b042831d68e90bd5
laney_ilyvie <laneyilyvie@gmail.com>
2026-04-16 13:36:59 +0200

    first work

--- upload_exam.php
+++ upload_exam.php
@@ -12,7 +12,7 @@ if ($_POST) {
     $file = "uploads/" . $_FILES['file']['name'];
     move_uploaded_file($_FILES['file']['tmp_name'], $file);
 
-    $conn->query("INSERT INTO assignments(title,description,file_path,teacher_id)
+    $conn->query("INSERT INTO assignments(id,title,description,file_path,teacher_id)
     VALUES('$_POST[title]','$_POST[description]','$file',1)");
 }
 ?>
