28513f79e8fd846316ba779e4787b789367b5328
laney_ilyvie <laneyilyvie@gmail.com>
2026-04-21 16:44:41 +0200

    Loggin

--- login.php
+++ login.php
@@ -4,17 +4,18 @@ session_start();
 ?>
 
 <form method="POST">
-    <input type="email" placeholder="Email" required><br>
+    <input type="text" name="name" placeholder="Name" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="password" placeholder="Password" required><br>
     <button>Login</button>
 </form>
 
 <?php
 if ($_POST) {
+    $name = $_POST['name'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    $result = $conn->query("SELECT * FROM students WHERE email='$email' AND password='$password'");
+    $result = $conn->query("SELECT * FROM students WHERE email='$name'");
     $row = $result->fetch_assoc();
 
     if ($row) {
