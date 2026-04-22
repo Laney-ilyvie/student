aabf9d60f32408a6ef7255310a5c797b4b06485d
laney_ilyvie <laneyilyvie@gmail.com>
2026-04-22 14:15:46 +0200

    assignments

--- view_assignment.php
+++ view_assignment.php
@@ -2,24 +2,15 @@
 include "db.php";
 session_start();
 
-// Check if student is logged in
-if (!isset($_SESSION['student_id'])) {
-    header("Location: login.php");
-    exit();
-}
 
-$student_id = $_SESSION['student_id'];
 
 $result = $conn->query ("SELECT * FROM assignments ");
-
-if ($result) {
-    while ($row = $result->fetch assoc) {
+ if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
         echo "<h3>" . $row['title'] . "</h3>";
         echo "<a href='" . $row['file_path'] . "' download>Download Assignment</a><br><br>";
-
+        
     }
-}
-
-
-
+ }
 ?>
+
